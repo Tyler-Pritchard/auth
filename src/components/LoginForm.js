7eaa6ad8c3865db81802d06554adb2428bf86909
Tyler-Pritchard <pritchard.tyler@gmail.com>
2019-06-06 23:02:22 -0700

    LoginForm added, app not crashing

---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class LoginForm extends Component {
+    render() {
+        return (
+            <View />
+        );
+    }
+}
+
+export default LoginForm;
