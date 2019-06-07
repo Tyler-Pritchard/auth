1a669a7a995b7cd4c2cddeb830df96d131e8fc94
Tyler-Pritchard <pritchard.tyler@gmail.com>
2019-06-06 22:56:25 -0700

    firebase set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component }  from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyAkDPnYxWqHYhbhofbeTDSE6WWCMdawoJA",
+            authDomain: "auth-7e18c.firebaseapp.com",
+            databaseURL: "https://auth-7e18c.firebaseio.com",
+            projectId: "auth-7e18c",
+            storageBucket: "auth-7e18c.appspot.com",
+            messagingSenderId: "649394455084",
+            appId: "1:649394455084:web:cb06790328fb218e"
+          });
+    }
+
     render() {
         return (
             <View>
