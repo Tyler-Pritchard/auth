3ced2cd4b8b27b9aba1e87d69e8aaa5e89060179
Tyler-Pritchard <pritchard.tyler@gmail.com>
2019-06-06 23:07:21 -0700

    Login form rendering

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
     render() {
         return (
-            <View />
+            <Card>
+                <CardSection />
+                <CardSection />
+
+                <CardSection>
+                    <Button>
+                        Log in
+                    </Button>
+
+                </CardSection>
+            </Card>
         );
     }
 }
