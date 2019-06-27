4ffff0c61f946be10d94c08c3e61080b82479b65
Tyler-Pritchard <pritchard.tyler@gmail.com>
2019-06-26 22:22:53 -0700

    refactor text argument in state to email

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-const Input = ({ label, value, onChangeText }) => {
+const Input = ({ label, value, onChangeText, placeholder }) => {
     const { inputStyle, labelStyle, containerStyle } = styles;
     return (
         <View style={containerStyle}>
-            <Text style={ labelStyle}>{label}</Text>
+            <Text style={labelStyle}>{label}</Text>
             <TextInput
+            placeholder={placeholder}
+            autoCorrect={false}
                 style={inputStyle}
                 value={value}
                 onChangeText={onChangeText}
-                style={{ height: 20, width: 100 }}
-                />
+                                />
         </View>
     );
 };
 
 const styles = {
     inputStyle: {
-        color: #000,
+        color: '#000',
         paddingRight: 5,
         paddingLeft: 5,
         fontSize: 18,
@@ -34,7 +35,7 @@ const styles = {
         height: 40,
         flex: 1,
         flexDirection: 'row',
-        align: 'center'
+        alignItems: 'center'
     }
 };
 
