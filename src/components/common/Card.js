f7513c73359097225e5550eec1e8c10316574a6c
Tyler-Pritchard <pritchard.tyler@gmail.com>
2019-06-26 21:04:42 -0700

    add form component

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -16,7 +16,7 @@ const styles = {
         borderColor: '#ddd',
         borderBottomWidth: 0,
         shadowColor: '#000',
-        shadowOffset: { widtht: 0, height: 2 },
+        shadowOffset: { width: 0, height: 2 },
         shadowOpacity: 0.1,
         shadowRadius: 2,
         elevation: 1,
